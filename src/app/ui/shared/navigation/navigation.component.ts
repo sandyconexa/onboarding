125162de148719db437ef9b7a6a17db64c50a258
Guillermo Savero <gsq2081@gmail.com>
2025-03-18 15:35:29 -0500

    feat

--- src/app/ui/shared/navigation/navigation.component.ts
+++ src/app/ui/shared/navigation/navigation.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [NzStepsModule, CommonModule],
+  imports: [NzStepsModule,NzTimelineModule ,NzGridModule,NzSpaceModule, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 
